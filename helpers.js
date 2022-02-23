c0dd5cf0ddce391b3c0cc4b148bb5e532d9d618f
Khalil ben zineb <khalil.benzineb@gmail.com>
2022-02-23 22:24:17 +0100

    02/23/22

---
+++ helpers.js
@@ -0,0 +1,67 @@
+const fs = require("fs");
+
+var resultHandler = function(err) { 
+	if(err) {
+		 console.log("unlink failed", err);
+	}
+}
+
+var deleteFolderContent = (folder) => {
+	var content = fs.readdirSync(folder);
+	for (const f of content) {
+		if (fs.lstatSync(folder+"/"+f).isDirectory()) {
+			// console.log("DIR " + f);
+			fs.rmSync(folder+"/"+f, { recursive: true });
+		} else {
+			// console.log(f);
+			fs.unlinkSync(folder+"/"+f);
+		}
+	}
+};
+
+var delay = (ms) => {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+var abjadTextMerge = (strA, strB) => {
+  // var yields = (strA.match(/(#ABJAD_EXTENDER_YIELD=.*#)/g) || []).map( key => key.slice(0, -1).replace("#ABJAD_EXTENDER_YIELD=", ""));
+  var finalData = strA;
+  var lines = strB.split('\n');
+  var nextSectionKey = null;
+  var nextOperation = "";
+  var nextData = [];
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i];
+    if((line.slice(0, "#ABJAD_EXTENDER_SECTION".length) == "#ABJAD_EXTENDER_SECTION") || i == lines.length-1){
+      if(i == lines.length-1){
+        if(nextSectionKey){
+          nextData.push(line);
+        }
+      }
+      if(nextSectionKey){
+        var res = nextData.join("\n");
+        if(nextOperation == "_APPEND") res = res + "#ABJAD_EXTENDER_YIELD="+nextSectionKey+"#";
+        if(nextOperation == "_PREPEND") res = "#ABJAD_EXTENDER_YIELD="+nextSectionKey+"#" + res;
+        finalData = finalData.replace(new RegExp("#ABJAD_EXTENDER_YIELD="+nextSectionKey+"#", 'g'), res);
+        nextSectionKey = null;
+        nextOperation = "";
+        nextData = [];
+      }
+      var tmp = line.replace("#ABJAD_EXTENDER_SECTION", "").split("=");
+      nextOperation = tmp[0].toUpperCase();
+      nextSectionKey = tmp[1];
+    }else{
+      if(nextSectionKey){
+        nextData.push(line);
+      }
+    }
+  }
+  return finalData;
+}
+
+module.exports = {
+  delay,
+  deleteFolderContent,
+  resultHandler,
+  abjadTextMerge,
+};
--- light-merger.js
+++ light-merger.js
@@ -2,56 +2,163 @@
 
 const chokidar = require('chokidar');
 const fs = require("fs");
+const path = require("path"); 
+const merge = require('deepmerge')
+const Helpers = require('./helpers.js');
 
-//Checking for config file
+//---------------------- GETTING PARAMS
+const Flags = {};// --log --no-watching --delete
+for (const arg of process.argv) {
+	if(arg.includes("--")){
+		// single True params
+		if(!arg.includes("=")){
+			Flags[arg.replace("--", "")] = true;
+		}else{
+			// Non Boolean params
+			Flags[arg.replace("--", "").split("=")[0]] = arg.replace("--", "").split("=")[1];
+		}
+		continue;
+	}
+	if(arg.includes("-")){
+		// single True params
+		if(!arg.includes("=")){
+			Flags[arg.replace("-", "")] = true;
+		}
+		continue;
+	}
+}
+
+
+//----------------------- HELP
+if(Flags.help || Flags.h){
+	console.log("Help:");
+	console.log("    ");
+	console.log("    Installation:");
+	console.log("    npm install -g ./");
+	console.log("    ");
+	console.log("    Options:");
+	console.log("    --log              Enable console logging");
+	console.log("    --no-watching      Create the final directory and quit");
+	console.log("    --delete           Delete destination folder's content before starting");
+	console.log("    --merge-files      Merge similar files");
+	console.log("                       JSON files   : [npm deepmerge] is used");
+	console.log("                       * files      : For yielding:");
+	console.log("                                      #ABJAD_EXTENDER_YIELD_APPEND=section_name");
+	console.log("                                      #ABJAD_EXTENDER_YIELD_PREPEND=section_name");
+	console.log("                                      #ABJAD_EXTENDER_YIELD_REPLACE=section_name");
+	console.log("                                    : For section creation:");
+	console.log("                                      #ABJAD_EXTENDER_SECTION=section_name for creating sections");
+	console.log("    --help (-h)        Show Help");
+	console.log("    ");
+	console.log("    COMMAND: lightmgr <OPTIONS>");
+	console.log("    ");
+	process.exit(1);
+}
+
+//----------------------- CHECKING FOR CONFIG FILE
 var rootDir = process.cwd();
 // console.log(rootDir);
 if (!fs.existsSync(rootDir+'/light-merger.json')) {
 	console.error("'light-merger.json' is missing!");
 	process.exit(1);
 }
-const config = JSON.parse(fs.readFileSync(rootDir+'/light-merger.json', 'utf8').replace(/@\//g, rootDir+"/").replace(/@/g, rootDir));
+// const config = JSON.parse(fs.readFileSync(rootDir+'/light-merger.json', 'utf8').replace(/@\//g, rootDir+"/").replace(/@/g, rootDir));
+const config = JSON.parse(fs.readFileSync(rootDir+'/light-merger.json', 'utf8'));
+
+//----------------------- INSTALLATION
+
 
 var watcher = null;
+const Files = {};
 var toWatchList = [];
-var finalDir = config.final.replace(/@\//g, rootDir + "/").replace(/@/g, rootDir);
+var toExcludeList = [];
+var toExcludeFilesList = [];
+var finalDir = (config.final || "").replace(/@\//g, rootDir + "/").replace(/@/g, rootDir);
+if(finalDir.slice(-1) == "/") finalDir = finalDir.slice(0, -1);
 
-var deleteFolderContent = (folder) => {
-	var content = fs.readdirSync(folder);
-	for (const f of content) {
-		if (fs.lstatSync(folder+"/"+f).isDirectory()) {
-			// console.log("DIR " + f);
-			fs.rmSync(folder+"/"+f, { recursive: true });
-		} else {
-			// console.log(f);
-			fs.unlinkSync(folder+"/"+f);
+var generateMultiLevelJSON = (path, newPath, mode) => {
+	var data;
+	try {
+		data = JSON.parse(fs.readFileSync(Files[newPath].files[0], 'utf8'));
+	} catch (error) {
+		console.log("--------------------- ERROR ----------------------");
+		console.log(error);
+		console.log("--------------------- _____ ----------------------");
+		throw "JSON ERROR in -> "+Files[newPath].files[0]
+	}
+	for (let i = 1; i < Files[newPath].files.length; i++) {
+		try {
+			data = merge(data, JSON.parse(fs.readFileSync(Files[newPath].files[i], 'utf8')));
+		} catch (error) {
+			console.log("--------------------- ERROR ----------------------");
+			console.log(error);
+			console.log("--------------------- _____ ----------------------");
+			throw "JSON ERROR in -> "+Files[newPath].files[i]
 		}
 	}
-};
-
-var resultHandler = function(err) { 
-	if(err) {
-		 console.log("unlink failed", err);
-	}
+  fs.writeFileSync(newPath, JSON.stringify(data, null, "\t"));
 }
 
-var delay = (ms) => {
-  return new Promise(resolve => setTimeout(resolve, ms));
+var generateMultiLevelTEXT = (path, newPath, mode) => {
+	var data;
+	data = fs.readFileSync(Files[newPath].files[0], 'utf8');
+	for (let i = 1; i < Files[newPath].files.length; i++) {
+		data = Helpers.abjadTextMerge(data, fs.readFileSync(Files[newPath].files[i], 'utf8'));
+	}
+  fs.writeFileSync(newPath, data);
+}
+
+var generateMultiLevelFile = (path, newPath, mode) => {
+	// Could be optimized for inital start to generate only once at the end by using the (mode & path) params
+  if(Files[newPath].type.toUpperCase() == "JSON" ){
+		generateMultiLevelJSON(path, newPath, mode);
+	}else{
+		generateMultiLevelTEXT(path, newPath, mode);
+	}
 }
 
 var getNewPath = (path) => {
-	return path.replace(toWatchList.filter(p => path.includes(p)).reduce((a, b) => (a.length > b.length) ? a : b), finalDir);
+	// Reduce is used because EXP: the file /parent/child/a.txt is in /parent/child and /parent and the longer is true
+	return path.replace(
+		toWatchList.filter((p) => {
+			return path.includes(p);
+		})
+		.reduce((a, b) => {
+			return (a.length > b.length) ? a : b;
+		}),
+		finalDir
+		);
 };
 
 var initWatcher = () => {
 	// console.log("initTMPWatcher() Started", 1);
 	// toWatchList.push(rootDir);
-	toWatchList = toWatchList.concat(config.merges.map(m => m.path.replace(/@\//g, rootDir + "/").replace(/@/g, rootDir)));
+	toWatchList = toWatchList.concat(
+		config.merges
+		.map(merge => {
+			var newPath = path.resolve(merge.path.replace(/@\//g, rootDir + "/").replace(/@/g, rootDir));
+			for (let i = 0; i < (merge.excludes || []).length; i++) {
+				var excludedPath = merge.excludes[i].replace(/@\//g, newPath + "/").replace(/@/g, newPath).replace(/\.\./g, "");
+				if(fs.existsSync(excludedPath) && fs.lstatSync(excludedPath).isDirectory()){
+					toExcludeList.push(excludedPath);
+				}else{
+					toExcludeFilesList.push(excludedPath);
+				}
+			}
+			return newPath;
+		})
+	);
+	toExcludeList = toExcludeList.concat((config.excludes || []).map(p => p.replace(/@\//g, rootDir + "/").replace(/@/g, rootDir)));
+	if(Flags["log"]) console.log("++++++++++++++++++++++  To Watch List  ++++++++++++++++++++++");
+	if(Flags["log"]) console.log(toWatchList);
+	if(Flags["log"]) console.log("++++++++++++++++++++++ To Exclude List ++++++++++++++++++++++");
+	if(Flags["log"]) console.log(toExcludeList);
 	watcher = chokidar.watch(toWatchList, {
 		ignored: (path, stat) => {
 			//Particular cases
+			// console.log("_______________________"+path);
 			if (path.includes(".goutputstream")) return true;
-			if ((config.excludes || []).map(p => p.replace(/@\//g, rootDir + "/").replace(/@/g, rootDir)).includes(path)) {
+			if (toExcludeList.includes(path)) {
 				// console.log("_________________ "+path);
 				return true;
 			}
@@ -65,46 +172,79 @@ var initWatcher = () => {
 	// var count = 0;
 	watcher
 		.on('add', async (path) => {
-			await delay(200);
+			if(toExcludeFilesList.includes(path)) return;
+			await Helpers.delay(200);
 			// count++;
 			// if (count > 3) process.exit(0);
-			fs.copyFile(path, getNewPath(path), fs.constants.COPYFILE_FICLONE, resultHandler);
-			console.log(`File ${path} has been added`);
+			if(Flags["merge-files"]) {
+				if(!Files[getNewPath(path)]){
+					Files[getNewPath(path)] = {
+						type: path.split('/').pop().split('.').pop(),
+						files: [
+							path
+						],
+					};
+					fs.copyFile(path, getNewPath(path), fs.constants.COPYFILE_FICLONE, Helpers.resultHandler);
+				}else{
+					Files[getNewPath(path)].files.push(path);
+					generateMultiLevelFile(path, getNewPath(path), "add");
+				}
+			}else{
+				fs.copyFile(path, getNewPath(path), fs.constants.COPYFILE_FICLONE, Helpers.resultHandler);
+			}
+			if(Flags["log"]) console.log(`File ${path} has been added`);
 			// if (watcherAvailable) this.updater("add", path);
 		})
 		.on('change', async (path) => {
-			await delay(200);
-			fs.copyFile(path, getNewPath(path), fs.constants.COPYFILE_FICLONE, resultHandler);
-			console.log(`File ${path} has been changed`);
+			if(Flags["no-watching"]) {
+				return;
+			}
+			if(toExcludeFilesList.includes(path)) return;
+			await Helpers.delay(200);
+			if(Flags["merge-files"]) {
+				if(Files[getNewPath(path)].files.length == 1){
+					fs.copyFile(path, getNewPath(path), fs.constants.COPYFILE_FICLONE, Helpers.resultHandler);
+				}else{
+					generateMultiLevelFile(path, getNewPath(path), "change");
+				}
+			}else{
+				fs.copyFile(path, getNewPath(path), fs.constants.COPYFILE_FICLONE, Helpers.resultHandler);
+			}
+			if(Flags["log"]) console.log(`File ${path} has been changed`);
 			// if (watcherAvailable) this.updater("change", path);
 		})
 		.on('unlink', path => {
 			try {
-				fs.rm(getNewPath(path), resultHandler);
+				fs.rm(getNewPath(path), Helpers.resultHandler);
 			} catch (error) {
 				console.error(error);
 			}
-			console.log(`File ${path} has been removed`);
+			if(Flags["log"]) console.log(`File ${path} has been removed`);
 			// if (watcherAvailable) this.updater("unlink", path);
 		})
 		.on('addDir', path => {
 			if (getNewPath(path) == finalDir) return;
-			fs.mkdir(getNewPath(path), resultHandler);
-			console.log(`Directory ${path} has been added`);
+			fs.mkdir(getNewPath(path), Helpers.resultHandler);
+			if(Flags["log"]) console.log(`Directory ${path} has been added`);
 			// if (watcherAvailable) this.updater("addDir", path);
 		})
 		.on('unlinkDir', path => {
-			console.log(`Directory ${path} has been removed`);
+			if(Flags["log"]) console.log(`Directory ${path} has been removed`);
 			fs.rmSync(getNewPath(path), { recursive: true });
 			// if (watcherAvailable) this.updater("unlinkDir", path);
 		})
-		.on('error', error => {
-			console.log(`Watcher error: ${error}`);
+		.on('error', (error) => {
+			if(Flags["log"]) console.log(`Watcher error: ${error}`);
 			// if (watcherAvailable) this.updater("error", path);
 		})
-		.on('ready', () => {
+		.on('ready', async () => {
+			await Helpers.delay(200);
 			// watcherAvailable = true;
 			// ('Initial scan complete. Ready for changes');
+			if(Flags["no-watching"]) {
+				watcher.close().then(() => console.log('All changes are done (without watching)'));
+				return;
+			}
 			console.clear();
 			console.log('________________________________________');
 			console.log('');
@@ -120,7 +260,7 @@ var initWatcher = () => {
 
 // Ensuring empty folder
 if (fs.existsSync(finalDir)) {
-	// deleteFolderContent(finalDir);
+	if(Flags.delete) Helpers.deleteFolderContent(finalDir);
 } else {
 	fs.mkdirSync(finalDir);
 }
